b217823c74088d5307cfb12ff83bdbbb0abc824c
vishalbhingardeve <85104362+vishalbhingardeve@users.noreply.github.com>
2021-11-21 13:06:38 +0530

    Vulnerability changes.

--- src/main/java/com/mindbowser/MindbowserAssignmentApplication.java
+++ src/main/java/com/mindbowser/MindbowserAssignmentApplication.java
@@ -1,7 +1,5 @@
 package com.mindbowser;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
